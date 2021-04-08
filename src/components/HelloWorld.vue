e3033d3fbab3981c537c4319b1ca4474b4cdcf0f
heinzaa <52783798+heinzaa@users.noreply.github.com>
2021-04-08 17:33:50 +0200

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+	<h1>{{ title }}</h1>
+	<h2>Take care of your daily calories intake</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return {
+        title: 'App Stock UI'
+      }
+    }
+  }
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
